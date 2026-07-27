2f281e1e86a02809cb000278760296711ce08ab4
Sehenaz <sehenaz.khatun.2000@gmail.com>
2026-07-27 17:38:59 +0530

    modified:   server.js

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ const PORT = process.env.PORT || 4000;
 
 mongoose.connect('mongodb://samaresh_Mondal:Mspl%402026@ac-yuzpvud-shard-00-00.xjccckc.mongodb.net:27017,ac-yuzpvud-shard-00-01.xjccckc.mongodb.net:27017,ac-yuzpvud-shard-00-02.xjccckc.mongodb.net:27017/mevtencia?authSource=admin&tls=true')
   .then(() => console.log('✅ MongoDB Atlas Connected!'))
-  .catch(err => { console.error('❌ MongoDB Error:', err); process.exit(1); });
+  .catch(err => { console.error('❌ MongoDB Error:', err);  });
 
-
+/////////////////////////////////////////////////////
 
 const employeeSchema = new mongoose.Schema({
   emp_id:         { type: String, required: true, unique: true },
